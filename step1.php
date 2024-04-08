87335710941c4dbec18f002db1195a8a93d94cfc
Stephdev48 <147699498+Stephdev48@users.noreply.github.com>
2024-04-08 16:58:26 +0200

    Balises form

---
+++ step1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Step1</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="">Nom</label><br>
+        <input type="text" name="nom"><br>
+        <label for="">Prénom</label><br>
+        <input type="text" name="prenom"><br>
+        <input type="submit" name="next" value="Suivant">
+    </form>
+</body>
+</html>
